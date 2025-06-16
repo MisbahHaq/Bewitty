cd015ec202e0da820ea660e22f749238d477f1f0
Misbah Haque <misbahmuneer2003@gmail.com>
2025-06-16 15:32:19 +0500

    Update

--- script.js
+++ script.js
@@ -3,4 +3,10 @@ var love = document.querySelector("i")
 
 con.addEventListener("dblclick", function () {
     love.style.transform = 'translate(-50%, -50%) scale(2)'
+    love.style.opacity = '0.8'
+
+    setTimeout(function () {
+        love.style.transform = 'translate(-50%, -50%) scale(0)'
+    }, 1000);
+
 })
